8a33789dd0309c6a65d1f2ee840bdb9a9268ac64
Avishek Prasad <avishekprasad0999@gmail.com>
2026-08-15 00:46:24 +0530

    Time: 44 ms (61.96%) | Memory: 54.6 MB (17.44%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function(word1, word2) {
+    let result="";
+    let len = word1.length + word2.length;
+
+    for(let i= 0 ; i<len;i++){
+
+        if(i<word1.length){
+            result+=word1[i];
+        }
+        if(i<word2.length){
+            result+=word2[i];
+        }
+
+       
+    }
+     return result;
+};
