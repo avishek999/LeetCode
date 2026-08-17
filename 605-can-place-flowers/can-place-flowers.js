901797e7af98256f5c47da8b7fe46dc0b72c27b2
Avishek Prasad <avishekprasad0999@gmail.com>
2026-08-17 14:15:52 +0530

    Time: 3 ms (33.93%) | Memory: 56 MB (64.35%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+
+    let leftEmptyPlot = false
+    let rightEmptyPlot = false
+    let counter = 0
+    
+    for(let i = 0 ;i < flowerbed.length ; i++ ){
+        if(flowerbed[i] === 0  ){
+            rightEmptyPlot = (i== 0) || (flowerbed[i-1] === 0);
+            leftEmptyPlot = (i== flowerbed.length -1) ||(flowerbed[i+1] ===0 );
+        }
+        if(rightEmptyPlot &&leftEmptyPlot  ){
+         flowerbed[i] = 1;
+         counter++
+         if(counter >= n){
+            return true
+         }
+        }
+    }
+
+      return counter >= n;
+};
