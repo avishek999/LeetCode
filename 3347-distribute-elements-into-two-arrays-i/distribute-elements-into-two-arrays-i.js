c9f5327ae295ecd6bc1604cea63eea618dda7234
Avishek Prasad <avishekprasad0999@gmail.com>
2026-08-30 22:55:41 +0530

    Time: 2 ms (60.58%) | Memory: 57 MB (64.78%) - LeetSync

---
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var resultArray = function(nums) {
+
+   
+    let arr1 = [nums[0]];
+    let arr2 = [nums[1]];
+
+    for(let i= 2 ;i <nums.length;i++){
+     if(arr1[arr1.length - 1] > arr2[arr2.length -1]){
+        arr1.push(nums[i])
+     }else{
+        arr2.push(nums[i]);
+     }
+    }
+
+    let result = [...arr1,...arr2];
+
+    return result;
+    
+};
